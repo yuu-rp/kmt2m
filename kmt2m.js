d280cf78dd604d5593aa5a4219d2fc8f198b328e
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-13 23:58:37 +0700

    Update kmt2m.js

--- kmt2m.js
+++ kmt2m.js
@@ -13,37 +13,37 @@
     'use strict';
 
     // Your code here...
-    document.addEventListener('touchstart', (e) => {
+    window.addEventListener('touchstart', (e) => {
 	    console.log("touchstart");
         e.preventDefault();
         e.stopImmediatePropagation();
         e.stopPropagation();
         const src = e.changedTouches[0];
-	    const ev = new PointerEvent("pointerdown", { pointerType:'mouse', x:src.clientX, y:src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0.5 });
+	    const ev = new PointerEvent("pointerdown", { isTrusted:true, pointerType:'mouse', x:src.clientX, y:src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0.5 });
 	    console.log(ev);
-	    e.target.dispatchEvent(ev);
+	    window.dispatchEvent(ev);
     });
 
-    document.addEventListener('touchend', (e) => {
+    window.addEventListener('touchend', (e) => {
 	    console.log("touchend");
         e.preventDefault();
         e.stopImmediatePropagation();
         e.stopPropagation();
         const src = e.changedTouches[0];
-        const ev = new PointerEvent("pointerup", { pointerType: 'mouse', x: src.clientX, y: src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0 });
+        const ev = new PointerEvent("pointerup", { isTrusted: true, pointerType: 'mouse', x: src.clientX, y: src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0 });
 	    console.log(ev);
-	    e.target.dispatchEvent(ev);
+        window.dispatchEvent(ev);
     });
 
-    document.addEventListener('touchmove', (e) => {
+    window.addEventListener('touchmove', (e) => {
     	console.log("touchmove");
         e.preventDefault();
         e.stopImmediatePropagation();
         e.stopPropagation();
         const src = e.changedTouches[0];
-        const ev = new PointerEvent("pointermove", { pointerType: 'mouse', x: src.clientX, y: src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0.5 });
+        const ev = new PointerEvent("pointermove", { isTrusted: true, pointerType: 'mouse', x: src.clientX, y: src.clientY, clientX: src.clientX, clientY: src.clientY, button:0, screenX: src.screenX, screenY: src.screenY, pressure:0.5 });
 	    console.log(ev);
-	    e.target.dispatchEvent(ev);
+        window.dispatchEvent(ev);
     });
 
 })();
