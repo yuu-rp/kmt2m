00e529f0c16f7bafc7ef81895a165b6fe96eb15a
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-13 17:51:25 +0700

    Update kmt2m.js

--- kmt2m.js
+++ kmt2m.js
@@ -1,75 +1,61 @@
 // ==UserScript==
-// @namespace     https://openuserjs.org/users/KvByte
-// @name          Kumon Connect touch to mouse
-// @description   Converts touch to mouse events, and adjust the interface for mobile.
-// @copyright     2021, KvByte (https://openuserjs.org/users/KvByte)
-// @license       MIT
-// @version       0.1.07
-// @include       https://kumonapp.digital.kumon.com/*
-// @icon          https://kumonapp.digital.kumon.com/th/favicon.ico
-// @grant         none
-// @noframes
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-13
+// @description  try to take over the world!
+// @author       You
+// @match        https://kumonapp.digital.kumon.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kumon.com
+// @grant        none
 // ==/UserScript==
 
-// ==OpenUserJS==
-// @author KvByte
-// ==/OpenUserJS==
+(function() {
+    'use strict';
 
-(function () {
-	'use strict';
-	console.log("Start User Script");
+    // Your code here...
+    document.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+        const src = e.changedTouches[0];
+        document.dispatchEvent( new PointerEvent('pointerdown', {
+                                     screenX: src.screenX,
+                                     screenY: src.screenY,
+                                     clientX: src.clientX,
+                                     clientY: src.clientY,
+                                     pressure:1
+                                     })
+        );
+    });
 
-	// Based on 'Mickey Shine' solution, created Tap to "click", and updated from initMouseEvent (deprecated) to MouseEvent (new)
-	// https://stackoverflow.com/questions/1517924/javascript-mapping-touch-events-to-mouse-events
-	let lastTouchStart = 0;
-	function touchToMouseHandler(event) {
-		// Get first touch input
-		let touches = event.changedTouches,
-			first = touches[0],
-			type = "";
+    document.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+        const src = e.changedTouches[0];
+        document.dispatchEvent( new PointerEvent('pointerup', {
+                                     screenX: src.screenX,
+                                     screenY: src.screenY,
+                                     clientX: src.clientX,
+                                     clientY: src.clientY,
+                                     pressure:0
+                                     })
+        );
+    });
 
-		// Get the correct type to simulate
-		switch (event.type) {
-			case "touchstart": type = "mousedown"; lastTouchStart = new Date().getTime(); break;
-			case "touchmove": type = "mousemove"; break;
-			case "touchend": type = "mouseup"; break;
-			case "touchend": type = "mouseup"; break;
-			default: return;
-		}
-
-		// dispatch the simulated type
-		const mouseEventInitDictionary = {
-			bubbles: true,
-			cancelable: true,
-			clientX: first.clientX,
-			clientY: first.clientY,
-			screenX: first.screenX,
-			screenY: first.screenY
-		}
-		const mouseEvent = new MouseEvent(type, mouseEventInitDictionary);
-		first.target.dispatchEvent(mouseEvent);
-
-		// Convert Tap to "click" event
-		if (type == "mouseup") {
-			const currentTime = new Date().getTime();
-			if (currentTime - lastTouchStart <= 200 /*ms*/) {
-				// dispatch the click type
-				const clickEvent = new MouseEvent("click", mouseEventInitDictionary);
-				first.target.dispatchEvent(clickEvent);
-			}
-		}
-
-		// cancels the bubbling of the touch event
-		event.preventDefault();
-	}
-
-	function initTouchToMouseConverter(el) {
-		el.addEventListener("touchstart", touchToMouseHandler, true);
-		el.addEventListener("touchmove", touchToMouseHandler, true);
-		el.addEventListener("touchend", touchToMouseHandler, true);
-		el.addEventListener("touchcancel", touchToMouseHandler, true);
-	}
-
-    initTouchToMouseConverter(document);
+    document.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+        const src = e.changedTouches[0];
+        document.dispatchEvent( new PointerEvent('pointermove', {
+                                     screenX: src.screenX,
+                                     screenY: src.screenY,
+                                     clientX: src.clientX,
+                                     clientY: src.clientY,
+                                     pressure:1
+                                     })
+        );
+    });
 
 })();
