b1a3af0ccb6b4eb049f09ebe4ebca29807e7711f
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-14 12:10:36 +0700

    Update k3.js

--- k3.js
+++ k3.js
@@ -18,9 +18,10 @@
     Object.defineProperty(proto, "addEventListener", {
         value: function (type, fn, ...rest) {
             _addEventListener.call(this, type, function(...args) {
-                if(type.startsWith('pointer') || type.startsWith('touch')) {
+                if(type.startsWith('pointer')) {
                     console.log(`addEventListener of type %c${type}`+` was fired`, "color:cyan; background-color:black; font-weight:bold; padding:0.2rem;");
-                    console.log('arguments:', args)
+                    console.log('arguments:', args[0]);
+                    if(args[0].pointerType=='touch') args[0].pointerType='pen';
                 }
                 return fn.apply(this, args);
             }, ...rest);
