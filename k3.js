7921696ee9af0aa1ed11f7875cd14dde1994ebae
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-14 12:16:49 +0700

    Update k3.js

--- k3.js
+++ k3.js
@@ -21,8 +21,8 @@
                 if(type.startsWith('pointer')) {
                     console.log(`addEventListener of type %c${type}`+` was fired`, "color:cyan; background-color:black; font-weight:bold; padding:0.2rem;");
                     console.log('arguments:', args[0]);
-                    if(args[0].pointerType=='touch') args[0].pointerType='pen';
-                    return fn.apply(this, [new PointerEvent({...args, pointerType:'pen'})]);
+                    // if(args[0].pointerType=='touch') args[0].pointerType='pen';
+                    return fn.apply(this, [new PointerEvent({...args[0], pointerType:'pen'})]);
                 }
                 return fn.apply(this, args);
             }, ...rest);
