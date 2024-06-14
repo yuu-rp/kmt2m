fdedbea923afd0042097bd618fec270462e8e6c5
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-14 12:24:44 +0700

    Update k3.js

--- k3.js
+++ k3.js
@@ -13,6 +13,13 @@
     'use strict';
 
     // Your code here...
+    function shallowClone(obj) {
+        return Object.create(
+            Object.getPrototypeOf(obj), 
+            Object.getOwnPropertyDescriptors(obj) 
+        );
+    }
+    
     const proto = EventTarget.prototype;
     const _addEventListener = EventTarget.prototype.addEventListener;
     Object.defineProperty(proto, "addEventListener", {
@@ -22,7 +29,7 @@
                     console.log(`addEventListener of type %c${type}`+` was fired`, "color:cyan; background-color:black; font-weight:bold; padding:0.2rem;");
                     console.log('arguments:', args[0]);
                     // if(args[0].pointerType=='touch') args[0].pointerType='pen';
-                    const _args = [{...args[0], pointerType:'pen'}]
+                    const _args = [{...shallowClone(args[0]), pointerType:'pen'}]
                     console.log(_args);
                     return fn.apply(this, _args);
                 }
