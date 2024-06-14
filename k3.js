eb0aca55766d49901fae13a68f5f18e106fc0ae0
yuu-rp <145643478+yuu-rp@users.noreply.github.com>
2024-06-14 12:04:31 +0700

    Update k3.js

--- k3.js
+++ k3.js
@@ -4,7 +4,7 @@
 // @version      2024-06-14
 // @description  try to take over the world!
 // @author       You
-// @match        https://kumonapp.digital.kumon.com/test/en/index.html
+// @match        https://kumonapp.digital.kumon.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kumon.com
 // @grant        none
 // ==/UserScript==
@@ -18,7 +18,10 @@
     Object.defineProperty(proto, "addEventListener", {
         value: function (type, fn, ...rest) {
             _addEventListener.call(this, type, function(...args) {
-                console.log(`addEventListener of type %c${type}`+` was fired`, "color:cyan; background-color:black; font-weight:bold; padding:0.2rem;");
+                if(type.startsWith('pointer') || type.startsWith('touch')) {
+                    console.log(`addEventListener of type %c${type}`+` was fired`, "color:cyan; background-color:black; font-weight:bold; padding:0.2rem;");
+                    console.log('arguments:', args)
+                }
                 return fn.apply(this, args);
             }, ...rest);
        }
